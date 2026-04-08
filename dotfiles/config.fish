98dc1cd599324bed2f6b1501414c5f120a72a2a1
firesquid6 <jdeiss06@gmail.com>
2026-04-07 19:22:25 -0500

    add spell

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -22,4 +22,6 @@ if status is-interactive
   export ANDROID_HOME="$HOME/Android/Sdk"
   export PATH="$PATH:$ANDROID_HOME/platform-tools"
   export PATH="$PATH:$HOME/.local/bin"
+
+  export PATH="$PATH:$HOME/.autosmith/bin"
 end
